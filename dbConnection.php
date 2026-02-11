f5a57588b1e1d6904b6ae42fb551c12d6193714c
John Kenly Pamor <130634805+stooopidkenly@users.noreply.github.com>
2026-02-11 22:14:35 +0800

    Clean up dbConnection.php by removing credentials
    Removed sensitive deployment information from dbConnection.php.

--- dbConnection.php
+++ dbConnection.php
@@ -12,19 +12,3 @@ try {
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-
-//practice deployment
-// password = PracticePortfolio_123
-// f32-preview.awardspace.net
-// 4710914
-
-//actual deployment
-// password = Portfolio_Pamor3E
-
-//host = fdb1032.awardspace.net
-
-//name = 4671494_portfolio
-// user = 	4671494_portfolio
-
-
-// ftp password = UYUHm#dD3nX*wqqq
